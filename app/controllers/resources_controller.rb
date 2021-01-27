b8c1d12e4f8be7c0b92990107a66ac6db6e55faa
Danielle <danisteele1984@gmail.com>
2021-01-27 18:38:55 +1100

    admin log in&out

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,6 +1,23 @@
 class ResourcesController < ApplicationController
+    before_action :check_for_login # :check_for_admin, :only => [:index]
 
     def index
         @resources = Resource.all 
     end
+
+    def new
+        @resource = Resource.new
+    end
+
+    def create
+        resource = Resource.create resource_params
+        @current_admin.resources << resource
+        redirect_to root_path
+    end
+
+    private
+    def resource_params
+        params.require(:resource).permit(:title)
+    end
+
 end
